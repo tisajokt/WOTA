da8c96386818415a0a576809e470cfbbe9ffff23
Tisajokt <james.and.tisa@gmail.com>
2018-10-01 11:12:11 -0400

    express install, basic index.html

--- server/app.js
+++ server/app.js
@@ -1,1 +1,10 @@
-// This is a test, and should be visible in the develop branch, but not in the master branch.
+
+// https://expressjs.com/en/starter/hello-world.html
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
