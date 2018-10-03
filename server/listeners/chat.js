65c5e78371c8d2e865e99df06d2b487e603413fb
Tisajokt <james.and.tisa@gmail.com>
2018-10-03 11:32:34 -0400

    Sanitize chat messages
    Sanitizing chat messages, by converting HTML special characters into their corresponding encoding, prevents XSS attacks. See https://www.owasp.org/index.php/XSS_(Cross_Site_Scripting)_Prevention_Cheat_Sheet.

--- server/listeners/chat.js
+++ server/listeners/chat.js
@@ -1,8 +1,30 @@
 
 module.exports = function chatListenerModule(env) {
 	
+	const htmlEncodes = {
+		"&": "&amp;",
+		"<": "&lt;",
+		">": "&gt;",
+		"\"": "&quot;",
+		"'": "&#x27;",
+		"/": "&#x2F;",
+		"\\": "&#92;"
+	};
+	function sanitize(message, replacements) {
+		var out = "";
+		for (let i = 0; i < message.length; i++) {
+			let thisReplacement = replacements[message[i]];
+			if (thisReplacement !== undefined) {
+				out += thisReplacement || "";
+			} else {
+				out += message[i];
+			}
+		}
+		return out;
+	};
+	
 	return function chatListener(socket, message, target) {
-		message = message.replace(/<.+?>/g, "");
+		message = sanitize(message, htmlEncodes);
 		if (target) {
 			env.io.to(target).emit("chat", socket.chatName, socket.chatColor, message);
 		} else {
